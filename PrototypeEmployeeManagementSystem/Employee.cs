a013f0fef45dbea9aa2664824603479e4d4b36c3
Matt <matt-jbl@hotmail.co.uk>
2022-11-21 10:06:48 +0000

    Employee base class created

---
+++ PrototypeEmployeeManagementSystem/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PrototypeEmployeeManagementSystem
+{
+    internal class Employee
+    {
+        public virtual float DayRate { get; }
+
+        public string Job { get; private set; }
+
+        public Employee(string job)
+        {
+            Job = job;
+        }
+
+        public void WorkNextShift()
+        {
+            if (BalanceSheet.PayEmployee(DayRate))
+            {
+                DoJob();
+            }
+        }
+        protected virtual void DoJob() { }
+
+    }   
+}
